a5a7ca3a66c9f77af0b8625886a2ca62a85295d6
Darius19920110 <meszidani22@gmail.com>
2022-07-14 00:56:32 +0100

    Adding user input functionality to store chosen brand urls

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ class Scraper():
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.URL = "https://www.myprotein.com/"
-        self.product_URL = []
+        self.brands_URL = []
+        self.brands_list = ["protein", "bars", "clothing", "vitamins", "vegan", "creatine"]
         self.driver.get(self.URL)
         self.delay = 10
 
     # Creating a wrapper for all functions
     def start_scraper(self):
        self.accept_cookies()
+       self.get_brands_url()
     
     # This function is accept cookies
     def accept_cookies(self):
@@ -41,6 +43,69 @@ class Scraper():
             time.sleep(1)
         except TimeoutException:
             print("Loading took too much time!")
+
+    # Creating a function of user input, and let the user to choose brand
+    def get_brands_url(self):
+        while True:
+            try: 
+                # GET USER CHOICE
+                if len(self.brands_URL) == 0:
+                    user_product_type = input(f'''
+                    ####### PLEASE SELECT WHICH PRODUCT YOU WANT TO GET DATA FROM #########
+                    #######################################################################
+                    ---> {self.brands_list} <---
+                    ''')
+                    print()
+                else:
+                    if len(self.brands_URL) == 6:
+                        break
+                    else:
+                        user_product_type = input(f'''
+                        ####### ADD MORE PRODUCTS OR PRESS "n" TO FINISH! #########
+                        #######################################################################
+                        ---> {self.brands_list} <---
+                        ''')
+                        print()
+
+                        if user_product_type.lower() == "n":
+                            break
+                
+                # Call function to get specific brand link and name
+                self.choose_brand(user_product_type.lower())
+     
+            except:
+                print("Invalid Selection!")
+    
+    # Get a link of a specific brand
+    def choose_brand(self, user_input):
+        index = 0
+        # Find the links index from a DOM of a chosen brand
+        if user_input == "protein":
+            index = 1
+        elif user_input == "bars":
+            index = 2
+        elif user_input == "clothing":
+            index = 3
+        elif user_input == "vitamins":
+            index = 4
+        elif user_input == "vegan":
+            index = 5
+        elif user_input == "creatine":
+            index = 6
+        
+        if user_input in self.brands_list:
+            link = self.driver.find_element(By.XPATH, f"//div[@class='brandLogos trackwidget componentWidget']/a[{index}]").get_attribute("href")
+
+            brand_object = {"brand": user_input, "link": link}
+            # Store brand details
+            self.brands_URL.append(brand_object)
+            i = self.brands_list.index(user_input)
+            # Delete the specific brand to make sure wont be chosen next time
+            del self.brands_list[i]
+        else:
+            # Reject input if brand chosen already or not valid!
+            print("This Product Is Already Selected or Not Valid")
+
 
 
 scraper = Scraper()
